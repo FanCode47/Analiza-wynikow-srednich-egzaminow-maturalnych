b2210a32ff2cc641c4e3e64ab555b6cdc7122add
Dmytro Pavlyk <fancode47@gmail.com>
2025-05-21 11:11:50 +0200

    ...
    ...

--- main.R
+++ main.R
@@ -1,4 +1,4 @@
-# Importowanie bibliotek --------------------------------------------------
+# Importowanie biblioteki readr --------------------------------------------------
 library(readr)
 
 # Ustawienie ścieżki folderu roboczego -----------------------------------
@@ -24,3 +24,64 @@ write_csv2(
   data_cleaned, 
   file.path(sciezka_folderu, "srednie_wyniki_cleaned.csv")  # Zapisujemy dane do nowego pliku w tym samym folderze
 )
+
+# Dzielenie ramek danych --------------------------------------------------
+# Ramka danych dla "ogółem"
+data_general <- data_cleaned[data_cleaned$plec == "ogółem", ]
+data_general$plec <- NULL
+# Ramka danych dla pozostałych płci
+data_by_gender <- data_cleaned[data_cleaned$plec != "ogółem", ]
+
+# Importowanie biblioteki ggplot2 --------------------------------------------------
+library(ggplot2)
+# Wykres słupkowy przedstawiający średnie wyniki z poszczególnych przedmiotów w różnych latach
+ggplot(data_general, aes(x = przedmiot, y = as.numeric(wartosc), fill = rok)) +
+  geom_col(position = "dodge") +
+  labs(title = "Średnie wyniki z przedmiotów (ogółem)", y = "Średni wynik", x = "Przedmiot") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+# Wykres słupkowy/boxplot z wynikami wg płci.
+ggplot(data_by_gender, aes(x = przedmiot, y = as.numeric(wartosc), fill = plec)) +
+  geom_boxplot() +
+  labs(title = "Porównanie wyników wg płci", y = "Wynik", x = "Przedmiot") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Importowanie biblioteki dplyr ------------------------------------------------
+library(dplyr)
+
+# Filtruj tylko matematykę
+math_trend <- data_general %>% filter(przedmiot == "matematyka")
+# Zmiana wyników w czasie (np. matematyka, język polski)
+ggplot(math_trend, aes(x = as.integer(rok), y = as.numeric(wartosc))) +
+  geom_line() +
+  geom_point() +
+  labs(title = "Trend wyników z matematyki w czasie", x = "Rok", y = "Średni wynik (%)")
+
+#  Analiza dysproporcji płci ----------------------------------------------
+library(tidyr)
+
+gender_diff <- data_by_gender %>%
+  group_by(rok, przedmiot, plec) %>%
+  summarise(wynik = mean(as.numeric(wartosc), na.rm = TRUE)) %>%
+  pivot_wider(names_from = plec, values_from = wynik) %>%
+  mutate(roznica = kobiety - mężczyźni)
+
+gender_diff <- na.omit(gender_diff)
+
+gender_diff_avg <- gender_diff %>%
+  group_by(przedmiot) %>%
+  summarise(srednia_roznica = mean(roznica))
+
+ggplot(gender_diff_avg, aes(x = reorder(przedmiot, srednia_roznica), y = srednia_roznica, fill = srednia_roznica > 0)) +
+  geom_col() +
+  coord_flip() +
+  labs(
+    title = "Średnia różnica wyników matur (kobiety - mężczyźni)",
+    x = "Przedmiot",
+    y = "Średnia różnica (%)"
+  ) +
+  scale_fill_manual(values = c("TRUE" = "tomato", "FALSE" = "steelblue"),
+                    name = "Kto wypadł lepiej?", labels = c("Mężczyźni", "Kobiety")) +
+  theme_minimal()
+
+
+
